05906115f55d8b2c4e73e1962b9444bfd74e1048
Rob Mayhew <rob@robmayhew.com>
2026-01-07 21:40:11 -0700

    Add another level

--- lightbear-game-jan-jan-2026/player.gd
+++ lightbear-game-jan-jan-2026/player.gd
@@ -14,7 +14,7 @@ var is_on_ladder = false
 var is_on_floor_tile = false
 var is_at_door = false
 var last_position:Vector2 = Vector2.ZERO
-
+var door_number = 0
 func _ready():
 	print("Ready")
 
@@ -58,7 +58,7 @@ func check_tile_properties() -> void:
 	is_on_ladder = false
 	is_on_floor_tile = false
 	is_at_door = false
-	
+	door_number = 0
 	var points:Array[Vector2i] = [
 		tile_pos,
 		#top_pos,
@@ -72,7 +72,9 @@ func check_tile_properties() -> void:
 				is_on_ladder = true
 			if tile_data.get_custom_data("door"):
 				is_at_door = true
-	
+			var d = tile_data.get_custom_data("door_number")
+			if d > 0:
+				door_number = d;
 	# Only check below for floor
 	var tile_data = tileMapLayer.get_cell_tile_data(bottom_pos)
 	if tile_data:
@@ -98,11 +100,11 @@ func handle_ladder_movement() -> void:
 	
 func handle_door_opening():
 	if Input.is_action_pressed("ui_up"):
-		print("Opening door from level ", level_id)
+		print("Opening door from level ", level_id, " id ", door_number)
 		if door_scenes.get(0):
-			print("Opening door to ", door_scenes.get(0).resource_path)
+			print("Opening door to ", door_scenes.get(door_number-1).resource_path)
 			is_at_door = false
-			SceneManager.change_scene(door_scenes.get(0).resource_path)
+			SceneManager.change_scene(door_scenes.get(door_number - 1).resource_path)
 
 func handle_floor_movement() -> void:
 	# Handle jump
